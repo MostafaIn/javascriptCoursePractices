7fadbe588565a9b3368fdd28d993c19c3f120b26
MostafaIn <mostafa.hazareh@integrify.fi>
2019-03-18 16:29:23 +0200

    passed destructuring lecture

--- JavaScript/script4.js
+++ JavaScript/script4.js
@@ -9,24 +9,24 @@ let firstName;
 console.log(firstName);
 firstName = 'Mostafa';
 console.log(firstName);
-let lastName='Hazareh';
+let lastName = 'Hazareh';
 console.log(lastName);
-let fullName= firstName +' '+ lastName;
+let fullName = firstName + ' ' + lastName;
 console.log(fullName);
 
 
 console.log(
-  "*****  Data Types: String, Number, Boolean, Null, undefined    *****"
+    "*****  Data Types: String, Number, Boolean, Null, undefined    *****"
 );
 
 // Data Types: String, Number, Boolean, Null, undefined
-let country= 'Finland';
+let country = 'Finland';
 let population = 6e6;
 console.log(country);
 console.log(population);
-let numOne= 4;
-let numTwo= 7;
-const gravity= 9.01;
+let numOne = 4;
+let numTwo = 7;
+const gravity = 9.01;
 let isMarried = true;
 let isRainy = false;
 console.log(numOne);
@@ -35,91 +35,91 @@ console.log(gravity);
 console.log(typeof country);
 console.log(typeof gravity);
 console.log(typeof isMarried);
-console.log(`The sum of ${numOne} and ${numTwo} is equal to ${numOne + numTwo}.` );
+console.log(`The sum of ${numOne} and ${numTwo} is equal to ${numOne + numTwo}.`);
 
 
 
 console.log(
-  "*****  Comparison Operators: <  >  >=  <=  == ===  !=  !==  *****"
+    "*****  Comparison Operators: <  >  >=  <=  == ===  !=  !==  *****"
 );
 
 // Comparison Operators: <  >  >=  <=  == ===  !=  !==
-console.log(4<3);
-console.log(7>4);
-console.log(7===7);
-console.log(7=='7');
-console.log( 7 != 7);
-console.log(7!== 7);
+console.log(4 < 3);
+console.log(7 > 4);
+console.log(7 === 7);
+console.log(7 === '7');
+console.log(7 != 7);
+console.log(7 !== 7);
 
 
 // Logical Operator: &&  ||  !()
-console.log(4< 3 || 10>9);
-console.log(3>2 && 9>4);
+console.log(4 < 3 || 10 > 9);
+console.log(3 > 2 && 9 > 4);
 console.log(!true);
 
 
 console.log("*****  Conditionals  *****");
 // Conditionals
-let x= 4;
-if(x>3){
+let x = 4;
+if (x > 3) {
     console.log("this block will run!");
 }
 
 let y = 1;
-if(y > 3){
+if (y > 3) {
     console.log("this is correct!");
-}else{
+} else {
     console.log("this is incorrect!!");
-    
+
 }
 
 
 console.log("*****  LOOP  *****");
 // LOOP
 // FOR LOOP  =>  for(initialization; condition; increment/decrement){}
-for(let i=0; i <= 100; i+=5 ){
-    console.log(i);  
+for (let i = 0; i <= 100; i += 5) {
+    console.log(i);
 }
 
-let allEvens= 0;
-let allOdds= 0;
-for (let j= 0; j<=20; j++){
-    if(j % 2 === 0){
+let allEvens = 0;
+let allOdds = 0;
+for (let j = 0; j <= 20; j++) {
+    if (j % 2 === 0) {
         allEvens = allEvens + j;
-        console.log(j +" is even.");
-    }else{
+        console.log(j + " is even.");
+    } else {
         allOdds = allOdds + j;
-        console.log(j +" is odd."); 
+        console.log(j + " is odd.");
     }
 }
-console.log("The sum of Even numbers = "+ allEvens);
-console.log("The sum of Odd numbers = "+ allOdds);
+console.log("The sum of Even numbers = " + allEvens);
+console.log("The sum of Odd numbers = " + allOdds);
 
 
 console.log("*****  Array  *****");
 // Array
-const num=[1,2,4,5,7,8,10,11];
+const num = [1, 2, 4, 5, 7, 8, 10, 11];
 console.log(num);
 console.log(num.length);
 
-let total=0;
-let average=0;
-for(let i=0; i<num.length; i++){
-    total+= num[i];
+let total = 0;
+let average = 0;
+for (let i = 0; i < num.length; i++) {
+    total += num[i];
 }
-console.log("the sum of numbers in array is = "+total);
-console.log("the average of numbers is = "+ total/num.length);
+console.log("the sum of numbers in array is = " + total);
+console.log("the average of numbers is = " + total / num.length);
 
 
 console.log("*****  Function  *****");
 //Function
-function square(num){
+function square(num) {
     console.log(num * num);
-    
+
 }
 square(3);
 
-function Sqr(num){
+function Sqr(num) {
     return num * num;
 }
 console.log(Sqr(4));
@@ -127,26 +127,26 @@ console.log(Sqr(5));
 console.log(Sqr(6));
 
 // this function shows a random number from 0 to 10
-function randomNum(){  
-    let rand= Math.floor(Math.random() *11);
+function randomNum() {
+    let rand = Math.floor(Math.random() * 11);
     return rand;
 }
 console.log(randomNum());
 
 // this function shows an array of 7 numbers(values) randomly from 0 to 10
-function randomArrayNum(){
-    let arr= [];
-    for(let i=0;i<7; i++){
-        arr[i]= Math.floor(Math.random()*11);
+function randomArrayNum() {
+    let arr = [];
+    for (let i = 0; i < 7; i++) {
+        arr[i] = Math.floor(Math.random() * 11);
     }
     return arr;
 }
 console.log(randomArrayNum());
 
 // function in one line
-const printFullName= (firstName= 'mostafa', lastName= 'hazareh') => firstName +" "+lastName;
+const printFullName = (firstName = 'mostafa', lastName = 'hazareh') => firstName + " " + lastName;
 console.log(printFullName());
-console.log(printFullName('johny','jacky'));
+console.log(printFullName('johny', 'jacky'));
 
 
 
@@ -159,7 +159,7 @@ console.log(printFullName('johny','jacky'));
 
 
 
- 
+
 
 
 
